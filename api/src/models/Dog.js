1e97453cf822c807d1b54fae37a60c0303f127bc
Carlos Martin Juncos <prof.mcjuncos@gmail.com>
2021-12-14 10:12:38 -0300

    141221_1012

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,51 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+  
+      },
+      name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-  });
+      validate: {
+        notEmpty: true
+      }
+      
+      },
+      height:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+        
+      },
+      weight:{
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+          notEmpty: true
+      }
+        
+      },
+      life_span:{
+        type: DataTypes.STRING,
+        allowNull: true, 
+        
+      },
+      image:{
+      type: DataTypes.STRING(100000),
+          
+      },
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+  
+      }
+    });
 };
